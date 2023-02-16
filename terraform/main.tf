597315749d93473c6509990e3d717650828ad188
apte-ame <apte.ame@northeastern.edu>
2023-02-15 20:24:02 -0500

    Updating terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-locals {
-  vpc_cidrs = [for number in range(0, "${var.vpc_count}") : cidrsubnet("${var.cidr}", 8, number)]
-}
+# locals {
+#   vpc_cidrs = [for number in range(0, "${var.vpc_count}") : cidrsubnet("${var.cidr}", 8, number)]
+# }
 
 module "myNetwork" {
   count  = var.vpc_count
   source = "./module/networking"
-  cidr   = local.vpc_cidrs[count.index]
-  # cidr = "${var.cidr}"
+  # cidr   = local.vpc_cidrs[count.index]
+  cidr = "${var.cidr}"
   vpc_count      = var.vpc_count
   priv_sub_count = var.priv_sub_count
   pub_sub_count  = var.pub_sub_count
