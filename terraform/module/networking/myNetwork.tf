921a8dbffaa6e320d405a8b1d2a75dcc4e805cab
apte-ame <apte.ame@northeastern.edu>
2023-02-15 20:38:45 -0500

    Updating Terraform Files

--- terraform/module/networking/myNetwork.tf
+++ terraform/module/networking/myNetwork.tf
@@ -73,7 +73,7 @@ resource "aws_route_table" "ameya_public_route_table" {
   ]
 }
 
-resource "aws_route" "aditya_public_route" {
+resource "aws_route" "ameya_public_route" {
   route_table_id         = aws_route_table.ameya_public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ameya_internet_gateway.id
