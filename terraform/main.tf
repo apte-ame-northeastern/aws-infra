c36ccd4c6587e1174239629b221d3b4e3b7d6921
apte-ame <apte.ame@northeastern.edu>
2023-04-06 04:44:48 -0400

    Adding loadbalancing and autoscaling

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,5 +28,25 @@ module "myNetwork" {
   db_name                 = var.db_name
   db_password             = var.db_password
   db_username             = var.db_username
+
+  asg_min_size                                 = var.asg_min_size
+  asg_max_size                                 = var.asg_max_size
+  asg_desired_capacity                         = var.asg_desired_capacity
+  asg_default_cooldown                         = var.asg_default_cooldown
+  scale_up_policy_cooldown                     = var.scale_up_policy_cooldown
+  scale_up_policy_scaling_adjustment           = var.scale_up_policy_scaling_adjustment
+  scale_down_policy_cooldown                   = var.scale_down_policy_cooldown
+  scale_down_policy_scaling_adjustment         = var.scale_down_policy_scaling_adjustment
+  cw_metric_alarm_scaleup_evaluation_periods   = var.cw_metric_alarm_scaleup_evaluation_periods
+  cw_metric_alarm_scaleup_period               = var.cw_metric_alarm_scaleup_period
+  cw_metric_alarm_scaleup_threshold            = var.cw_metric_alarm_scaleup_threshold
+  cw_metric_alarm_scaledown_evaluation_periods = var.cw_metric_alarm_scaledown_evaluation_periods
+  cw_metric_alarm_scaledown_period             = var.cw_metric_alarm_scaledown_period
+  cw_metric_alarm_scaledown_threshold          = var.cw_metric_alarm_scaledown_threshold
+  health_check_healthy_threshold               = var.health_check_healthy_threshold
+  health_check_unhealthy_threshold             = var.health_check_unhealthy_threshold
+  health_check_timeout                         = var.health_check_timeout
+  health_check_interval                        = var.health_check_interval
+
 }
 
